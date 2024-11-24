3ce5deed0b6215500d0143d4033d5bdabbea748a
Isuri <tmsisuri.rathnayake@gmail.com>
2024-11-24 23:40:56 +0530

    constant in javascript

---
+++ Constants.js
@@ -0,0 +1,10 @@
+let redius = 5
+const pi = 3.14
+let area
+
+area = pi*redius*redius
+console.log(area)
+
+//Variables defined with const cannot be Redeclared
+//Variables defined with const cannot be Reassigned
+
