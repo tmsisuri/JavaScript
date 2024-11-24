b8cb1f4df0eb69729f8fc33cb78a59a5ab725174
Isuri <tmsisuri.rathnayake@gmail.com>
2024-11-24 23:25:33 +0530

    add comments

--- variables.js
+++ variables.js
@@ -1,7 +1,15 @@
+/*variable name
+   can have characters
+   can have numbers except first position
+   can have special symbols $ and _
+*/
 let num = 4
 console.log(num)
 
+//when assign a string use quotes('') or ("")
 let username = "Sara"
 console.log(username)
+let username1 = 'Anne'
+console.log(username1)
 
 let useraccount = "Sara's account"
